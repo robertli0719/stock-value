4dc581d2fccfa6d26089b0cde6d84dc0e039c673
Robert Li <li.liufv@gmail.com>
2019-05-20 10:52:18 -0700

    add roa

--- main.py
+++ main.py
@@ -25,18 +25,20 @@ title_arr = ["Trailing P/E", "Forward P/E",
 data_set = data.pick_data(ticker_arr, title_arr)
 
 print("\nInstrments", "\tTrailing PE", "\tForward PE",
-      "\tP/B", "\tRoE", "\tCost Years")
+      "\tP/B", "\tRoA", "\tRoE", "\tCost Years")
 
 for ticker in data_set:
     vals = data_set[ticker]
     t_pe = vals["Trailing P/E"]
     f_pe = vals["Forward P/E"]
     roe_str = vals["Return on Equity"]
+    roa_str = vals["Return on Assets"]
     roe = p2f(roe_str)
+    roa = p2f(roa_str)
     pb = float(vals["Price/Book"])
     cost_year = round(math.log(pb)/math.log(1+roe), 2)
 
-    row_format = "{:<10}\t{:<7}\t\t{:<7}\t\t{:<7}\t{:<7}\t{:>7}"
+    row_format = "{:<10}\t{:<7}\t\t{:<7}\t\t{:<7}\t{:<7}\t{:>7}\t{:>7}"
 
-    teams_list = [ticker, t_pe, f_pe, pb, roe_str, cost_year]
+    teams_list = [ticker, t_pe, f_pe, pb, roa_str, roe_str, cost_year]
     print(row_format.format(*teams_list))
